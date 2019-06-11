75a3cca8974d80e66c092967c46ff0aed240b858
spuiln <251531091@qq.com>
2019-06-11 14:33:00 +0800

    test

---
+++ src/main/java/com/caogen/jfd/entity/SysUser.java
@@ -0,0 +1,9 @@
+package com.caogen.jfd.entity;
+
+import java.io.Serializable;
+
+public class SysUser implements Serializable {
+
+	private static final long serialVersionUID = 644322004791503324L;
+
+}
