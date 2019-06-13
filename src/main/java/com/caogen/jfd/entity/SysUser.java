78be34fdd23310bebab7a158623627bb78f453b4
spuiln <251531091@qq.com>
2019-06-13 18:21:15 +0800

    Signed-off-by: spuiln <251531091@qq.com>

--- src/main/java/com/caogen/jfd/entity/SysUser.java
+++
@@ -1,9 +0,0 @@
-package com.caogen.jfd.entity;
-
-import java.io.Serializable;
-
-public class SysUser implements Serializable {
-
-	private static final long serialVersionUID = 644322004791503324L;
-
-}
