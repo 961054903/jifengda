d6cc6634304b403c3499fd5bea9d46243b5cc0f1
spuiln <251531091@qq.com>
2019-06-21 09:57:45 +0800

    Signed-off-by: spuiln <251531091@qq.com>

--- src/main/java/com/caogen/jfd/entity/user/AppUserTicket.java
+++ src/main/java/com/caogen/jfd/entity/user/AppUserTicket.java
@@ -1,7 +1,6 @@
 package com.caogen.jfd.entity.user;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
 
 /**
  * 
@@ -13,20 +12,9 @@ public class AppUserTicket implements Serializable {
 	private static final long serialVersionUID = -1216643014979706211L;
 	private Integer id;
 	private String phone;
-	private String title;
-	private String description;
-	private Double money;
-	private Type type;
-	private Double sill;
-	private LocalDateTime start_date;
-	private LocalDateTime end_data;
-	private Double duration;
+	private String ticket;
 	private Boolean available;
 	private Reason reason;
-
-	public enum Type {
-		common, condition
-	}
 
 	public enum Reason {
 		past, use
@@ -48,9 +36,8 @@ public class AppUserTicket implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Ticket [id=" + id + ", title=" + title + ", description=" + description + ", money=" + money + ", type="
-				+ type + ", sill=" + sill + ", start_date=" + start_date + ", end_data=" + end_data + ", duration="
-				+ duration + ", available=" + available + ", reason=" + reason + ", phone=" + phone + "]";
+		return "AppUserTicket [id=" + id + ", phone=" + phone + ", ticket=" + ticket + ", available=" + available
+				+ ", reason=" + reason + "]";
 	}
 
 	public Integer getId() {
@@ -61,68 +48,20 @@ public class AppUserTicket implements Serializable {
 		this.id = id;
 	}
 
-	public String getTitle() {
-		return title;
+	public String getPhone() {
+		return phone;
 	}
 
-	public void setTitle(String title) {
-		this.title = title;
+	public void setPhone(String phone) {
+		this.phone = phone;
 	}
 
-	public String getDescription() {
-		return description;
+	public String getTicket() {
+		return ticket;
 	}
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public Double getMoney() {
-		return money;
-	}
-
-	public void setMoney(Double money) {
-		this.money = money;
-	}
-
-	public Type getType() {
-		return type;
-	}
-
-	public void setType(Type type) {
-		this.type = type;
-	}
-
-	public Double getSill() {
-		return sill;
-	}
-
-	public void setSill(Double sill) {
-		this.sill = sill;
-	}
-
-	public LocalDateTime getStart_date() {
-		return start_date;
-	}
-
-	public void setStart_date(LocalDateTime start_date) {
-		this.start_date = start_date;
-	}
-
-	public LocalDateTime getEnd_data() {
-		return end_data;
-	}
-
-	public void setEnd_data(LocalDateTime end_data) {
-		this.end_data = end_data;
-	}
-
-	public Double getDuration() {
-		return duration;
-	}
-
-	public void setDuration(Double duration) {
-		this.duration = duration;
+	public void setTicket(String ticket) {
+		this.ticket = ticket;
 	}
 
 	public Boolean getAvailable() {
@@ -139,14 +78,6 @@ public class AppUserTicket implements Serializable {
 
 	public void setReason(Reason reason) {
 		this.reason = reason;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
 	}
 
 }
