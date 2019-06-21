0a2e44f4ad6b7adf4882a3f51d5bc5c277b9582d
spuiln <251531091@qq.com>
2019-06-21 09:18:46 +0800

    Signed-off-by: spuiln <251531091@qq.com>

--- src/main/java/com/caogen/jfd/entity/user/AppUserTicket.java
+++ src/main/java/com/caogen/jfd/entity/user/AppUserTicket.java
@@ -1,6 +1,7 @@
 package com.caogen.jfd.entity.user;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * 
@@ -9,15 +10,30 @@ import java.io.Serializable;
  */
 public class AppUserTicket implements Serializable {
 
-	private static final long serialVersionUID = -5915627962332462479L;
+	private static final long serialVersionUID = -1216643014979706211L;
 	private Integer id;
-	private Integer ticket_id;
+	private String phone;
+	private String title;
+	private String description;
+	private Double money;
+	private Type type;
+	private Double sill;
+	private LocalDateTime start_date;
+	private LocalDateTime end_data;
+	private Double duration;
 	private Boolean available;
 	private Reason reason;
-	private String phone;
+
+	public enum Type {
+		common, condition
+	}
 
 	public enum Reason {
 		past, use
+	}
+
+	public AppUserTicket() {
+		super();
 	}
 
 	public AppUserTicket(Integer id) {
@@ -32,8 +48,9 @@ public class AppUserTicket implements Serializable {
 
 	@Override
 	public String toString() {
-		return "AppUserTicket [id=" + id + ", ticket_id=" + ticket_id + ", available=" + available + ", reason="
-				+ reason + ", phone=" + phone + "]";
+		return "Ticket [id=" + id + ", title=" + title + ", description=" + description + ", money=" + money + ", type="
+				+ type + ", sill=" + sill + ", start_date=" + start_date + ", end_data=" + end_data + ", duration="
+				+ duration + ", available=" + available + ", reason=" + reason + ", phone=" + phone + "]";
 	}
 
 	public Integer getId() {
@@ -44,12 +61,68 @@ public class AppUserTicket implements Serializable {
 		this.id = id;
 	}
 
-	public Integer getTicket_id() {
-		return ticket_id;
+	public String getTitle() {
+		return title;
 	}
 
-	public void setTicket_id(Integer ticket_id) {
-		this.ticket_id = ticket_id;
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getMoney() {
+		return money;
+	}
+
+	public void setMoney(Double money) {
+		this.money = money;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+	public Double getSill() {
+		return sill;
+	}
+
+	public void setSill(Double sill) {
+		this.sill = sill;
+	}
+
+	public LocalDateTime getStart_date() {
+		return start_date;
+	}
+
+	public void setStart_date(LocalDateTime start_date) {
+		this.start_date = start_date;
+	}
+
+	public LocalDateTime getEnd_data() {
+		return end_data;
+	}
+
+	public void setEnd_data(LocalDateTime end_data) {
+		this.end_data = end_data;
+	}
+
+	public Double getDuration() {
+		return duration;
+	}
+
+	public void setDuration(Double duration) {
+		this.duration = duration;
 	}
 
 	public Boolean getAvailable() {
