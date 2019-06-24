f905b6638e7a204ec9460d9113765ec95adac1b6
javapuls <2698622129@qq.com>
2019-06-24 15:11:02 +0800

    修改

--- src/main/java/com/caogen/jfd/service/driver/PersonalServicelmpl.java
+++ src/main/java/com/caogen/jfd/service/driver/PersonalServicelmpl.java
@@ -1,18 +1,31 @@
 package com.caogen.jfd.service.driver;
 
 
+import com.caogen.jfd.dao.driver.AppDriverDao;
+import com.caogen.jfd.dao.driver.CompleteDao;
 import com.caogen.jfd.dao.driver.PersonalDao;
 
+import com.caogen.jfd.entity.driver.AppDriver;
+import com.caogen.jfd.entity.driver.Complete;
 import com.caogen.jfd.entity.driver.Personal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
 public class PersonalServicelmpl implements PersonalService {
     @Autowired
     private PersonalDao personalDao;
+    @Autowired
+    private CompleteDao completeDao;
+
+    @Autowired
+    private AppDriverDao appDriverDao;
+
+
 
 
     @Override
@@ -75,6 +88,21 @@ public class PersonalServicelmpl implements PersonalService {
     public void getmodify(Personal cities) {
 
         personalDao.update1(cities);
+    }
+    @Override
+    public Personal getchampion() {
+            AppDriver appDriver = new AppDriver();
+            Complete complete = new Complete();
+            Personal personal = new Personal();
+            List<Complete> completes = completeDao.find4();
+            Complete complete1 = completes.get(0);
+            Integer driver_id = complete1.getCc();
+            appDriver.setId(driver_id);
+            AppDriver app = appDriverDao.get(appDriver);
+            String  phone =  app.getDriverphone();
+            personal.setPhone(phone);
+        Personal personalDao6 = personalDao.get6(personal);
+        return personalDao6;
     }
 
 
