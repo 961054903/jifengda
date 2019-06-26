e018e52cb86d2a5eb2531f9a5fd4b116138f7f10
javapuls <2698622129@qq.com>
2019-06-26 15:45:18 +0800

    ..

---
+++ src/main/java/com/caogen/jfd/dao/driver/DriverSitDao.java
@@ -0,0 +1,8 @@
+package com.caogen.jfd.dao.driver;
+
+import com.caogen.jfd.dao.BaseDao;
+import com.caogen.jfd.entity.driver.DriverSite;
+
+public interface DriverSitDao extends BaseDao<DriverSite> {
+
+}
