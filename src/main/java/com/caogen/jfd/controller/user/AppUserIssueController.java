803aff9bdbae5dd02ec5daa7c37c5bb62561b941
javapuls <2698622129@qq.com>
2019-07-11 08:27:44 +0800

    ....

--- src/main/java/com/caogen/jfd/controller/user/AppUserIssueController.java
+++ src/main/java/com/caogen/jfd/controller/user/AppUserIssueController.java
@@ -74,7 +74,7 @@ public class AppUserIssueController {
 	 * @return
 	 */
 	@ResponseBody
-	@RequestMapping(value = { "history", "api/history" })
+	@RequestMapping(value = { "history", "app/history" })
 	public Message history(String data) {
 		Message message = new Message();
 		try {
