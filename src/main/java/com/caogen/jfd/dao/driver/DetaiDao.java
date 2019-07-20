0300057e6cf4d56322555a9b0cfc99ce01148455
spuiln <251531091@qq.com>
2019-07-20 11:42:02 +0800

    Signed-off-by: spuiln <251531091@qq.com>

--- src/main/java/com/caogen/jfd/dao/driver/DetaiDao.java
+++
@@ -1,8 +0,0 @@
-package com.caogen.jfd.dao.driver;
-
-
-import com.caogen.jfd.dao.BaseDao;
-import com.caogen.jfd.entity.driver.Detail;
-
-public interface DetaiDao extends BaseDao<Detail> {
-}
--- src/main/java/com/caogen/jfd/dao/driver/TimeDao.java
+++
@@ -1,9 +0,0 @@
-package com.caogen.jfd.dao.driver;
-
-import com.caogen.jfd.dao.BaseDao;
-import com.caogen.jfd.entity.driver.Time;
-
-
-public interface TimeDao extends BaseDao<Time> {
-
-}
