e2074626f50b70c0563fc7673a436e3e10cbe612
spuiln <251531091@qq.com>
2019-07-20 12:00:01 +0800

    Signed-off-by: spuiln <251531091@qq.com>

--- src/main/java/com/caogen/jfd/dao/driver/FeedBackDao.java
+++
@@ -1,7 +0,0 @@
-package com.caogen.jfd.dao.driver;
-
-import com.caogen.jfd.dao.BaseDao;
-import com.caogen.jfd.entity.driver.FeedBack;
-
-public interface FeedBackDao extends BaseDao<FeedBack>{
-}
--- src/main/java/com/caogen/jfd/service/driver/OnlineSeriver.java
+++
@@ -1,8 +0,0 @@
-package com.caogen.jfd.service.driver;
-
-import com.caogen.jfd.entity.driver.Online;
-import com.caogen.jfd.service.BaseService;
-
-public interface OnlineSeriver  extends BaseService<Online> {
-    void in(Integer driver_id, Online.Operation operation);
-}
